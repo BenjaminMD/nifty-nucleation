e0496f345bd258b962c7cc095db0368ce6f9bd59
Benjamin Fahl <benjamin@129-066.eduroam.rwth-aachen.de>
2024-02-22 14:25:36 +0100

    code formatting

--- main.py
+++ main.py
@@ -4,18 +4,86 @@ and config file generate solution. Aggregation averaging keeps species low.
 
 Author: Benjamin Fahl
 """
-from model import NucleationGrowthModel, solve_agg, solve_nuc_gr_agg 
-from rsc.utils import read_config, save_results
+from glob import glob
+
+import matplotlib.pyplot as plt
+import numpy as np
+from ezplot import plot_defaults
+
+from model import NucleationGrowthModel, solve_agg, solve_nuc_gr_agg
+from rsc.utils import read_config, read_results, save_results
+
+
+def number_vs_size(df, ax):
+    R_col = df.columns[df.columns.str.startswith("R")]
+    P_col = df.columns[df.columns.str.startswith("P")]
+
+    df = df.iloc[-1]
+    P = df[P_col].to_numpy()
+    P = P / P.sum()
+    R = df[R_col].to_numpy() * 1e9
+
+    ax.hist(R, bins=20, weights=P, alpha=0.5, label="Size distribution")
+
+    return ax
+
+
+def compute_average_size(R, P):
+    total = 0
+    sum_of_products = 0
+    for i in range(len(R)):
+        total += P[i]
+        sum_of_products += R[i] * P[i]
+    average_size = sum_of_products / total
+    return average_size
+
+
+def e_gr(e_grs):
+    mean_diameter = []
+    for e in e_grs:
+        config = read_config()
+        config["activation"]["E_gr"] = e
+        model = NucleationGrowthModel(config)
+        agg_sol = solve_agg(agg=model.K.k_agg, **config["sim_params"]).sol
+        results = solve_nuc_gr_agg(model, config)
+        save_results(results, model, config)
+        file_paths = glob("./results/*/*.h5")
+        file_paths.sort()
+        df = read_results(file_paths[-1])
+        R_col = df.columns[df.columns.str.startswith("R")]
+        P_col = df.columns[df.columns.str.startswith("P")]
+
+        df = df.iloc[-1]
+        P = df[P_col].to_numpy()
+        P = P / P.sum()
+        R = df[R_col].to_numpy() * 1e9
+        mean_diameter.append(compute_average_size(R, P))
+    return mean_diameter
+
+
+def run_model():
+    config = read_config()
+    model = NucleationGrowthModel(config)
+    
+    agg_sol = solve_agg(agg=model.K.k_agg, **config["sim_params"]).sol
+    results = solve_nuc_gr_agg(model, config, agg_sol)
+    save_results(results, model, config)
+    file_paths = glob("./results/*/*.h5")
+    file_paths.sort()
+    df = read_results(file_paths[-1])
+
+    fig, gs = plot_defaults(1, 1)
+    ax = fig.add_subplot(gs[0])
+    number_vs_size(df, ax)
+    plt.show()
 
 
 def main():
-    config = read_config()
-    model = NucleationGrowthModel(config)
-    model.K.k_agg
-    agg_sol = solve_agg(agg=model.K.k_agg, **config['sim_params']).sol
+    config = read_config('config.toml') ##either give here a path to the toml config file or leave empty if it is in the same directory
+    model = NucleationGrowthModel(config) ##initialize the model with the starting parameters
+    print(model.K.k_agg)
+    agg_sol = solve_agg(agg=0, **config['sim_params']).sol
     results = solve_nuc_gr_agg(model, config, agg_sol)
-    model.__init__(config)
-    
     save_results(results, model, config)
 
 if __name__ == "__main__":
--- model/solve.py
+++ model/solve.py
@@ -1,47 +1,56 @@
-from model import apply_agg_avg
-
+import numpy as np
 from scipy import integrate
 from tqdm import tqdm
-import numpy as np
+
+from model import apply_agg_avg
 
 
 def solve_nuc_gr_agg(model, config, agg_sol=None):
     data = []
- 
+
     values = list(model.c.values())
     y0 = np.concatenate([np.array(values[:-1]), values[-1]])
-    ode = integrate.RK45(model.model, y0=y0, **config['sim_params'])
+    ode = integrate.RK45(model.model, y0=y0, **config["sim_params"])
+
 
     progress_bar = tqdm(total=1e2, mininterval=0.5, unit_scale=True)
-    while ode.status == 'running':
-
+    while ode.status == "running":
+        if ode.t > 1:
+           ode.max_step = 0.1 
+        if ode.t > 10:
+           ode.max_step = 1 
+        if ode.t > 100:
+           ode.max_step = 1e6 
+        if ode.t > 1000:
+           ode.max_step = 1e6 
         ode.step()
-        
-        model.var['t_nuc'][model.J] = ode.t
-        model.prev['R'] = model.var['R'].copy()
-        model.prev['P'] = model.var['P'].copy()
-        model.prev['Au0_gr'] = np.array(model.ddt['Au0_gr']).copy()
 
+        model.var["t_nuc"][model.J] = ode.t
+        model.prev["R"] = model.var["R"].copy()
+        model.prev["P"] = model.var["P"].copy()
+        model.prev["Au0_gr"] = np.array(model.ddt["Au0_gr"]).copy()
 
         paramavg = {
-            't': ode.t, 't_old': ode.t_old, 'sol': agg_sol,
-        } | {key: model.var[key] for key in ['P', 'P_nuc', 'R', 't_nuc']}
-        
-        # ---- Aggregation ----
-        if agg_sol: model.var['P'], model.var['R'] = apply_agg_avg(**paramavg)
-        # ---- Aggregation ----
-        
-        model.J += 1 if model.var['P'][model.J] > 1e-14 else 0
-        if model.J == config['const']['max_cohorts']: break
+            "t": ode.t,
+            "t_old": ode.t_old,
+            "sol": agg_sol,
+        } | {key: model.var[key] for key in ["P", "P_nuc", "R", "t_nuc"]}
 
-        data.append([
-            ode.t, model.var['S'], *ode.y, *model.var['R'], *model.var['P']
-        ])
+        # ---- Aggregation ----
+        if agg_sol:
+            model.var["P"], model.var["R"] = apply_agg_avg(**paramavg)
+        # ---- Aggregation ----
 
-        progress = (ode.t - ode.t_old)
-        progress /= config['sim_params']['t_bound']
+        model.J += 1 if model.var["P"][model.J] > 1e-14 else 0
+        if model.J == config["const"]["max_cohorts"]:
+            break
+
+        data.append([ode.t, model.var["S"], *ode.y, *model.var["R"], *model.var["P"]])
+
+        progress = ode.t - ode.t_old
+        progress /= config["sim_params"]["t_bound"]
         progress *= 0.9999e2
-        
+
         progress_bar.update(progress)
     progress_bar.close()
     return data
